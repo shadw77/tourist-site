127d0f2605d8676a586e54138e94da0968d249d8
abdelrahman ahmed <abdelrahmanahmedtarek1999@gmail.com>
2023-11-02 22:06:24 +0200

    comment on guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -101,7 +101,7 @@ const routes: Routes = [
      {
       path:'discover',loadChildren: () => import('src/app/discovermodule/discovermodule.module')
       .then(m=>m.DiscovermoduleModule),
-      canActivate:[authGuard,adminOrVendorGuard]
+      //canActivate:[authGuard,adminOrVendorGuard]
     },
     
     {
