f8e4157c351685056100deca33b7bd15cf0d4e49
“hager”‏‏ <hagersalah249@gmail.com>
2023-11-02 03:01:01 +0200

    updating on restaurant

--- src/app/database/restaurants/restaurants.component.ts
+++ src/app/database/restaurants/restaurants.component.ts
@@ -9,7 +9,7 @@ import{ RestaurantCrudService } from 'src/app/Services/restaurant-crud.service';
 export class RestaurantsComponent {
 
   Restaurants: any = [];
-
+  imagePath: string = 'http://127.0.0.1:8000/images/Restaurant_images/thumbnails/';
   constructor(private restaurantCrudService: RestaurantCrudService){}
   ngOnInit():void{
     this.restaurantCrudService.getRestaurants()  .subscribe(res=>{        
@@ -23,8 +23,44 @@ export class RestaurantsComponent {
     this.restaurantCrudService.deleteRestaurant(id).subscribe(res=>{
       this.Restaurants.splice(i,1);
   })
-    
+  this.restaurantCrudService.getRestaurants().subscribe(res=>{        
+            this.Restaurants= res;
+       }); 
    
   }
-
+ 
 }
+
+//import { Component } from '@angular/core';
+// import{ HotelCrudService } from 'src/app/Services/hotel-crud.service';
+
+// @Component({
+//   selector: 'app-hotels',
+//   templateUrl: './hotels.component.html',
+//   styleUrls: ['./hotels.component.css']
+// })
+// export class HotelsComponent {
+
+//   Hotels: any = [];
+//   imagePath: string = 'http://127.0.0.1:8000/images/Hotel_images/thumbnails/';
+
+//   constructor(private hotelCrudService: HotelCrudService){}
+//   ngOnInit():void{
+//     this.hotelCrudService.getHotels().subscribe(res=>{        
+//         this.Hotels= res;
+//         // console.log(this.Hotels['data']);
+//     });
+   
+//   }
+
+//   delete(id:any){
+//     console.log(id);
+//     this.hotelCrudService.deleteHotel(id).subscribe(res=>{
+//       console.log('delete');
+//       this.hotelCrudService.getHotels().subscribe(res=>{        
+//         this.Hotels= res;
+//     });
+//   })
+//   }
+
+// }
