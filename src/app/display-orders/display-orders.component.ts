cc2e0ae4b07d202ed3f4a24701a8aa9b4cca3b43
shadw77 <Shady2018170763@cis.asu.edu.eg>
2023-11-04 18:22:50 +0200

    git orders for specific user

--- src/app/display-orders/display-orders.component.ts
+++ src/app/display-orders/display-orders.component.ts
@@ -9,12 +9,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class DisplayOrdersComponent {
   Orders: any = [];
+  latestOrder:any=[];
   userData:any;
+  serviceName: any=[];
+  imagePaths:any;
+  imagePath:any;
+  userName:any;
+  ProcessedOrders: any[] = [];
 
   constructor(private userOrderCrudService: UserOrderCrudService,  private http: HttpClient,
     ){}
   
   ngOnInit():void{
+
     const userDataString = localStorage.getItem('userData');
     if (userDataString) {
        this.userData = JSON.parse(userDataString);
@@ -23,11 +30,54 @@ export class DisplayOrdersComponent {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${this.userData.api_token}`,
     });
+    this.userName =this.userData.name;
+
+    this.userOrderCrudService.getAllUserOrders(this.userData.id).subscribe(res => {        
+      this.Orders = res;
+      this.latestOrder = Object.values(res); // Convert the object values to an array      
+
+      this.latestOrder = this.latestOrder[0].sort((a: any, b: any) => {
+        const timestampA = new Date(a.created_at).getTime();
+        const timestampB = new Date(b.created_at).getTime();      
+        return timestampB - timestampA;
+      });
       
-    this.userOrderCrudService.getAllUserOrders(this.userData.id).subscribe(res=>{        
-        this.Orders= res;
-        console.log(this.Orders.data);
+      const latestTimestamp = new Date(this.latestOrder[0].created_at).getTime(); // Get the timestamp of the latest order
+
+      this.latestOrder = this.Orders.data.filter((order: any) => {
+        const timestamp = new Date(order.created_at).getTime();
+        return timestamp === latestTimestamp;
+      });
+        if (this.latestOrder.length > 0) {
+          this.ProcessedOrders.push(this.latestOrder[0]);
+            this.Orders.data = this.Orders.data.filter((order: any)=> order !== this.latestOrder[0]);
+                this.latestOrder = [];
+        }
+   
+    
+      console.log(this.latestOrder);
+      console.log(this.Orders.data);
+      const currentTime = new Date().getTime();
+      const timeDifference = currentTime - latestTimestamp;
+      const minutesPassed = timeDifference / (1000 * 60);
+
+      if (minutesPassed >= 30) {
+        this.Orders.data.push(...this.ProcessedOrders);
+        this.ProcessedOrders = [];
+      }
     });
+        this.imagePaths=  {
+      Hotel: 'http://127.0.0.1:8000/images/Hotel_images/thumbnails/',
+      Restaurant: 'http://127.0.0.1:8000/images/Restaurant_images/thumbnails/',
+      Trip: 'http://127.0.0.1:8000/images/trip_images/thumbnails/',
+      Destination: 'http://127.0.0.1:8000/images/destination_images/thumbnails/'
+    };
+  for (let i = 0; i < this.Orders.data.length; i++) {
+    const currentProductType = this.Orders.data[i].service_type;
+    const currentImagePath = this.imagePaths[currentProductType]; 
+    this.imagePath = currentImagePath;
+  }
+
   }
 
   delete(id:any, i:any){
@@ -38,5 +88,7 @@ export class DisplayOrdersComponent {
     
    
   }
+
+  
 
 }
